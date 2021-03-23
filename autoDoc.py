50b464f249973a7023864f3e775764bd0bed2086
va3wam <va3wam@gmail.com>
2021-03-23 08:57:15 -0400

    Added comment stating that ths script is now depricated

--- autoDoc.py
+++ autoDoc.py
@@ -2,7 +2,9 @@
 # @file autoDoc.py
 # @author the Aging Apprentice
 # @brief Twipe2 robot firmware 
-# @details Basic operating code for the Twipe2 robot 
+# @details Basic operating code for the Twipe2 robot. This file is not used 
+# anymore. We now use Github actions to generate documentation at commit time
+# which is better than generating it every time we run a build.  
 # @copyright Copyright (c) 2021 the Aging Apprentice
 # Permission is hereby granted, free of charge, to any person obtaining a copy of 
 # this software and associated documentation files (the "Software"), to deal in the 
