29c76b623f24931aebe0239631edef78f3b14c30
va3wam <va3wam@gmail.com>
2021-03-30 21:13:19 -0400

    Added MQTT library and moved network related variables around.

--- src/main.h
+++ src/main.h
@@ -10,16 +10,16 @@
  ************************************************************************************/
 #include <Arduino.h> // Arduino Core for ESP32. Comes with Platform.io.
 #include <aaChip.h> // Core (CPU) details that the code running on.
-#include <aaNetwork.h> // Required for handling Wifi functions. 
+#include <aaNetwork.h> // Wifi functions. 
 #include <aaWebService.h> // Realtime web-based network config and OTA code updates.
 #include <aaFlash.h> // Use Flash memory to store values that persist past reboot.
+#include <aaMqtt.h> // Use MQTT for remote management and monitoring.
 
 /************************************************************************************
  * @section mainConstants Define constant values. 
  ************************************************************************************/
 const char* HOST_NAME_PREFIX = "Twipe2"; // Prefix for our unique network name.
 const char* WEB_APP_TITLE = "Twipe2"; // App name for web page titles.
-IPAddress brokerIP; // IP address of the MQTT broker.
 
 /************************************************************************************
  * @section mainInstantiate Instantiated objects.
@@ -29,20 +29,24 @@ aaChip appCpu; // Access information about the ESP32 application microprocessor 
 aaNetwork network(HOST_NAME_PREFIX); // WiFi session management.
 aaWebService localWebService(WEB_APP_TITLE); // Webserver hosted by microcontroller.
 aaFlash flash; // Non-volatile memory management. 
+aaMqtt mqtt; // Publish and subscribe to MQTT broker. 
 
 /************************************************************************************
  * @section mainGlobalVar Declare global variables.
  ************************************************************************************/
 bool isWebServer; // True is web server running.
+IPAddress brokerIP; // IP address of the MQTT broker.
+char uniqueName[HOST_NAME_SIZE]; // Character array that holds unique name for Wifi network purposes. 
+char *uniqueNamePtr = &uniqueName[0]; // Pointer to first address position of unique name character array.
 
 /************************************************************************************
  * @section mainDeclare Declare functions.
  ************************************************************************************/
-// Declare functions.
 void setupSerial(); // Initialize the serial output.
 void showCfgDetails(); // Show the environment details of this application.
 void startWebServer(); // Start up the local web server service.
 void setup(); // Arduino mandatory function #1. Runs once at boot. 
 void loop(); // Arduino mandatory function #2. Runs continually.
+void monitorWebServer(); // Look after pending web server requests.
 
 #endif // End of precompiler protected code block
