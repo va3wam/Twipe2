c5ef18e04352d9eb51c9873d67ffa619e65b04c6
va3wam <va3wam@gmail.com>
2021-03-30 21:10:36 -0400

    Fix line length of comments.

--- lib/libTemplate/libTemplate.cpp
+++ lib/libTemplate/libTemplate.cpp
@@ -1,48 +1,56 @@
-/*************************************************************************************************************************************
+/************************************************************************************
  * @file libTemplate.cpp
  * @author theAgingApprentice
- * @brief Common API for retrieving ESP32 chip configuration information. 
- * @details Retrieve chip configuration details from both FreeRTOS and the ESP32 Arduino framework.
+ * @brief Put brief description here with only one period at the end. 
+ * @details Put detailed description here. Can have multiple periods and can also span
+ * multiple lines.
  * @copyright Copyright (c) 2021 the Aging Apprentice
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files 
- * (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, 
- * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished 
- * to do so, subject to the following conditions:
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
- * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of 
+ * this software and associated documentation files (the "Software"), to deal in the 
+ * Software without restriction, including without limitation the rights to use, copy, 
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+ * and to permit persons to whom the Software is furnished to do so, subject to the 
+ * following conditions: The above copyright notice and this permission notice shall 
+ * be included in all copies or substantial portions of the Software. THE SOFTWARE 
+ * IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE 
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE 
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.
  * 
  * YYYY-MM-DD Dev        Description
- * ---------- ---------- -------------------------------------------------------------------------------------------------------------
- * 2021-03-17 Old Squire Program created.
- *************************************************************************************************************************************/
+ * ---------- ---------- ------------------------------------------------------------
+ * 2021-03-29 Old Squire Program created.
+ ************************************************************************************/
 #include <libTemplate.h> // Header file for linking.
 
 /**
  * @brief This is the default constructor for this class.
-===================================================================================================*/
-libTemplate::libTemplate() 
+ * @param const char* Argument description.
+ * =================================================================================*/
+libTemplate::libTemplate(const char* var1) 
 {
-   Serial.println("<libTemplate::libTemplate> aaNetwork default constructor running.");
+   // Serial.print does not work in constructor. Do not use.
 } //libTemplate::libTemplate()
 
 /**
  * @brief This is the second form of the constructor for this class.
-===================================================================================================*/
-libTemplate::libTemplate(const char* var1)
+ * @param const char* Argument description.
+ * @param const char* Argument description.
+ * =================================================================================*/
+libTemplate::libTemplate(const char* var1,  const char* var2)
 {
-   Serial.println("<libTemplate::libTemplate> aaNetwork second form constructor running.");
+   // Serial.print does not work in destructor. Do not use.
 } //libTemplate::libTemplate()
 
 /**
  * @brief This is the destructor for this class.
-===================================================================================================*/
+ * =================================================================================*/
 libTemplate::~libTemplate() 
 {
    Serial.println("<libTemplate::libTemplate> aaNetwork destructor running.");
-} //libTemplate::libTemplate()
+} //libTemplate::~libTemplate()
 
 /**
  * @brief Explain briefly what this is.
@@ -50,9 +58,9 @@ libTemplate::~libTemplate()
  * @param uint8_t var1 Explain what this argument is for.
  * @param uint8_t var2 Explain what this argument is for.
  * @return boolean value.     
-===================================================================================================*/
+ * =================================================================================*/
 bool libTemplate::exampleFunction(uint8_t var1, uint8_t var2)
 {
    Serial.println("<libTemplate::exampleFunction> Put variables particuar "); 
    return true; 
-} //libTemplate::showCfgDetails()
+} //libTemplate::exampleFunction()
--- lib/libTemplate/libTemplate.h
+++ lib/libTemplate/libTemplate.h
@@ -4,14 +4,22 @@
 #ifndef libTemplate_h // Start of precompiler check to avoid dupicate inclusion of this code block.
 #define libTemplate_h // Precompiler macro used for precompiler check.
 
+/************************************************************************************
+ * @section mainIncludes Included libraries.
+ ************************************************************************************/
 #include <Arduino.h> // Arduino Core for ESP32. Comes with Platform.io
 
-// Define libTemplate class
+/************************************************************************************
+ * @class Describe the class here.
+ * @brief Write a brief description in one sentence that ends with a period.
+ * @details Write a longer explanation of the class if needed. Can be multiple 
+ * sentences. Can also be multiple lines.   
+ ************************************************************************************/
 class libTemplate // Indicate that we are extending LiquidCrystal_I2C class with our class 
 {
    public:
-      libTemplate(); // Default constructor for this class.
-      libTemplate(const char* var1); // Second form of class constructor.
+      libTemplate(const char* var1); // Default constructor for this class.
+      libTemplate(const char* var1, const char* var2); // Second form of class constructor.
       ~libTemplate(); // Class destructor.
       bool exampleFunction(uint8_t var1, uint8_t var2); // This is how yoou define a function
    private:
